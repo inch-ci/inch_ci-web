b433b1ed887ed02f27abd3a4d53a4a21042c7390
René Föhring <rf@bamaru.de>
2014-05-28 22:57:06 +0200

    Truncate tag_uid in labels

--- app/presenters/revision_presenter.rb
+++ app/presenters/revision_presenter.rb
@@ -8,4 +8,13 @@ class RevisionPresenter < BasePresenter
   def uid(short = true)
     short ? revision.uid[0..7] : revision.uid
   end
+
+  def tag_uid(short = true, count = 10)
+    return unless revision.tag_uid
+    if short && revision.tag_uid.size > 10
+      revision.tag_uid[0...count] + '...'
+    else
+      revision.tag_uid
+    end
+  end
 end
