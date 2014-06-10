116ecf1aeaca97e88cd62769723fe0ceaf2dc2b1
René Föhring <rf@bamaru.de>
2014-06-10 09:02:24 +0200

    Add missing require

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,3 +1,4 @@
 require 'inch_ci/config'
 require 'inch_ci/store'
+require 'inch_ci/project_uid'
 require 'inch_ci/worker/project'
--- test/inch_ci/project_uid_test.rb
+++ test/inch_ci/project_uid_test.rb
@@ -1,7 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
-require 'inch_ci/project_uid'
-
 describe ::InchCI::ProjectUID do
   fixtures :all
 
