25b4aded56c8883fe3177eaf698bd9aa97eb0d39
René Föhring <rf@bamaru.de>
2014-07-31 10:35:19 +0200

    Add CodeObjectsControllerTest

--- test/controllers/code_objects_controller_test.rb
+++ test/controllers/code_objects_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CodeObjectsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all
+
+  test "should get :show with existing revision" do
+    rev = '8849af2b7ad96c6aa650a6fd5490ef83629faf2a'
+    short_rev = rev[0..6]
+    [rev, short_rev].each do |revision_uid|
+      %w(html js).each do |format|
+        params = {
+          :service => 'github',
+          :user => 'rrrene',
+          :repo => 'sparkr',
+          :branch => 'master',
+          :revision => revision_uid,
+          :code_object => '1',
+          :format => format
+        }
+        get :show, params
+        assert_response :success
+      end
+    end
+  end
 end
