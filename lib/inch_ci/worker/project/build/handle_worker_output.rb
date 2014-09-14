54026d78d75c34e88e947a838398a03a55ad7056
René Föhring <rf@bamaru.de>
2014-09-14 23:01:53 +0200

    Handle dirty worker output

--- lib/inch_ci/worker/project/build/handle_worker_output.rb
+++ lib/inch_ci/worker/project/build/handle_worker_output.rb
@@ -7,13 +7,24 @@ module InchCI
       module Build
         class HandleWorkerOutput
           def initialize(stdout, stderr, build = nil, save_service = SaveBuildData)
-            data = YAML.load(stdout)
+            data = handle_stdout(stdout)
             if data && result = data['build']
               save_service.call(build, result)
             else
               debug = {:stdout => stdout, :stderr => stderr}
               raise "Running worker ".color(:red) + build.inspect.color(:cyan) +  " failed:".color(:red) + " #{debug.inspect}"
             end
+          end
+
+          private
+
+          # Returns the last part of the given +output+, where YAML
+          # is defined.
+          # @param output [String,nil]
+          # @return [String,nil]
+          def handle_stdout(output)
+            yaml = output =~ /^(---\n\S+\:\n.+)/m && $1
+            yaml && YAML.load(yaml)
           end
         end
       end
--- test/inch_ci/worker/project/build/handler_worker_output_test.rb
+++ test/inch_ci/worker/project/build/handler_worker_output_test.rb
@@ -24,4 +24,42 @@ describe ::InchCI::Worker::Project::Build::HandleWorkerOutput do
       described_class.new(output, stderr, build, FakeSaveBuildData)
     end
   end
+
+  it 'should work with output littered with YARD warnings' do
+    output = <<-STDOUT
+[error]: NameError: uninitialized constant YARD::Handlers::Ruby::PrivateClassMethodHandler::NamespaceMissingError
+[error]: Stack trace:
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/ruby/private_class_method_handler.rb:32:in `rescue in privatize_class_method'
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/ruby/private_class_method_handler.rb:25:in `privatize_class_method'
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/ruby/private_class_method_handler.rb:11:in `block (2 levels) in <class:PrivateClassMethodHandler>'
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/ruby/private_class_method_handler.rb:8:in `each'
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/ruby/private_class_method_handler.rb:8:in `block in <class:PrivateClassMethodHandler>'
+  /home/user/.rvm/gems/ruby-2.1.2@inch_ci/gems/yard-0.8.7.4/lib/yard/handlers/processor.rb:114:in `block (2 levels) in process'
+
+[warn]: YARD will recover from this error and continue to parse but you *may* have problems
+[warn]: with your generated documentation. You should probably fix this.
+[warn]: -
+---
+build:
+  status: success
+  repo_url: https://github.com/puppetlabs/puppet.git
+  branch_name: master
+  started_at: &1 2014-09-14 22:49:58.645222890 +02:00
+  finished_at: *1
+  latest_revision: true
+  service_name: github
+  user_name: puppetlabs
+  repo_name: puppet
+  badge_in_readme: true
+  inch_version: 0.5.0.rc6
+  revision_uid: ab15dc8812b22069d035116842b1cd0360671997
+  revision_message: "(PUP-3144) Skip the openbsd service specs on windows"
+  revision_author_name: Kylo Ginsberg
+  revision_author_email: kylo@puppetlabs.com
+  revision_authored_at: Fri Sep 12 17:34:17 2014 -0700
+  objects: []
+STDOUT
+    stderr = ''
+    described_class.new(output, stderr, build, FakeSaveBuildData)
+  end
 end
