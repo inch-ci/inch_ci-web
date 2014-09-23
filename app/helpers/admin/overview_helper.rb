8e7eea2e91bc9f28b7f390f996c4aedbbd26a483
René Föhring <rf@bamaru.de>
2014-09-23 11:53:39 +0200

    Mark projects in admin as of today/yesterday

--- app/helpers/admin/overview_helper.rb
+++ app/helpers/admin/overview_helper.rb
@@ -1,2 +1,10 @@
 module Admin::OverviewHelper
+  def created_at_classes(project)
+    today = Time.now.utc.midnight
+    created = project.created_at.midnight
+    [
+      created == today ? 'today' : nil,
+      created == today - 1 ? 'yesterday' : nil,
+    ]
+  end
 end
