6a7b35a6730c52f1b021018f51ba593977bcc659
René Föhring <rf@bamaru.de>
2014-12-22 00:00:22 +0100

    Add styles to badge selector and reposition badge

--- app/services/badge_markup.rb
+++ app/services/badge_markup.rb
@@ -2,27 +2,34 @@ class BadgeMarkup < Struct.new(:project, :branch)
   BASE_URL = "http://inch-ci.org"
   IMAGE_FORMATS = [:svg, :png]
   DEFAULT_IMAGE_FORMAT = IMAGE_FORMATS.first
+  IMAGE_STYLES = ['flat', 'flat-square', nil]
 
-  def each(format = DEFAULT_IMAGE_FORMAT, &block)
-    format_map(format).each(&block)
+  def each(format = DEFAULT_IMAGE_FORMAT, style = nil, &block)
+    format_map(format, style).each(&block)
   end
 
   def image_formats
     IMAGE_FORMATS
   end
 
-  def image_path(format = DEFAULT_IMAGE_FORMAT)
-    "#{page_path}.#{format}?branch=#{branch.name}"
+  def image_path(format = DEFAULT_IMAGE_FORMAT, style = nil)
+    base = "#{page_path}.#{format}?branch=#{branch.name}"
+    base << "&style=#{style}" if style
+    base
   end
 
-  def image_url(format = DEFAULT_IMAGE_FORMAT)
-    "#{BASE_URL}#{image_path(format)}"
+  def image_url(format = DEFAULT_IMAGE_FORMAT, style = nil)
+    "#{BASE_URL}#{image_path(format, style)}"
+  end
+
+  def styles
+    IMAGE_STYLES
   end
 
   private
 
-  def format_map(format)
-    image = image_url(format)
+  def format_map(format, style = nil)
+    image = image_url(format, style)
     link  =  page_url
     alt   = "Inline docs"
     {
