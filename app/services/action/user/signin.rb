a510e94ee6747cf1119e74cf2dfd8e7759af636c
René Föhring <rf@bamaru.de>
2014-12-26 22:17:07 +0100

    Fix access_token retrieval

--- app/services/action/user/signin.rb
+++ app/services/action/user/signin.rb
@@ -13,36 +13,29 @@ module Action
       def initialize(request)
         @user = find_or_create_user(request.env["omniauth.auth"])
         if @user.last_signin_at.nil?
-          sync_and_build_projects
+          # sync_and_build_projects
         end
-        update_user @user, request.params
       end
 
       private
 
-      def follows
-        client = Octokit::Client.new(:access_token => @user.github_access_token)
-        list = client.following(@user.user_name)
+      def follows(user)
+        client = Octokit::Client.new(:access_token => user.github_access_token)
+        list = client.following(user.user_name)
         list.map { |h| h['login'] }
       end
 
       def find_or_create_user(auth)
-        ::User.find_or_create_with_omniauth(auth)
+        user = ::User.find_or_create_with_omniauth(auth)
+        update_user user, auth
+        user
       end
 
-      def github_access_token(params)
-        client = Octokit::Client.new
-        token_response = client.exchange_code_for_token params[:code], CLIENT_ID, CLIENT_SECRET
-        token_response['access_token']
-      end
-
-      def update_user(user, params)
-        attributes = {
-          :github_access_token => github_access_token(params),
-          :last_signin_at => Time.now,
-        }
-        attributes[:follows] = follows if @user.follows.nil?
-        user.update_attributes(attributes)
+      def update_user(user, auth)
+        user.github_access_token = auth["credentials"]["token"]
+        user.last_signin_at = Time.now
+        user.follows = follows(user) if user.follows.nil?
+        user.save
       end
     end
   end
