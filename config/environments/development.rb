ebef65dfd27fbf1b211ab01fdd22188a54451ab7
René Föhring <rf@bamaru.de>
2015-01-11 00:47:13 +0100

    Update localizations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,10 @@ InchCI::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    #Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-  #  Bullet.growl = true
+    #Bullet.growl = true
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
