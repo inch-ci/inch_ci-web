d84c5b5d7a8348b0a87458498fa606e386a22111
René Föhring <rf@bamaru.de>
2015-01-15 18:48:59 +0100

    Do not load uninitialized projects on users#show

--- app/services/action/user/show.rb
+++ app/services/action/user/show.rb
@@ -30,7 +30,7 @@ module Action
       end
 
       def find_projects(user)
-        InchCI::Store::FindAllProjects.call(user)
+        InchCI::Store::FindAllProjects.call(user).select(&:name)
       end
     end
   end
