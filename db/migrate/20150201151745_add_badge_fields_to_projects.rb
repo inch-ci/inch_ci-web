fa7109dcafbb80e932ef657f6f7714192880dcce
René Föhring <rf@bamaru.de>
2015-02-01 16:32:03 +0100

    Add badge related fields to projects

---
+++ db/migrate/20150201151745_add_badge_fields_to_projects.rb
@@ -0,0 +1,8 @@
+class AddBadgeFieldsToProjects < ActiveRecord::Migration
+  def change
+    add_column :projects, :badge_generated, :boolean
+    add_column :projects, :badge_filled_in_percent, :integer
+    add_column :projects, :badge_in_readme_added_at, :datetime
+    add_column :projects, :badge_in_readme_removed_at, :datetime
+  end
+end
