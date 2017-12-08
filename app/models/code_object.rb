3ce1cc77853ec5ec2a0bdd10e3df58b369fab8dc
René Föhring <rene.foehring@5minds.de>
2017-12-08 07:14:20 +0100

    Remove emojis before saving docstrings

--- app/models/code_object.rb
+++ app/models/code_object.rb
@@ -8,4 +8,16 @@ class CodeObject < ActiveRecord::Base
   accepts_nested_attributes_for :code_object_roles
 
   validate :fullname, :presence => true, :uniqueness => {:scope => :revision}
+
+  before_save :remove_emojis_before_save
+
+  private
+
+  def remove_emojis_before_save
+    self.docstring = self.class.remove_emojis(self.docstring)
+  end
+
+  def self.remove_emojis(string)
+    string.gsub(/[\u{1F600}-\u{1F6FF}]/, "")
+  end
 end
